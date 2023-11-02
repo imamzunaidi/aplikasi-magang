f390fa98e43b8af04ef61d95920b7d2cfcb64202
imam9 <imamzunaidi4@gmail.com>
2023-11-02 20:37:27 +0700

    perubahan terakhir

--- resources/views/template_admin/footer.blade.php
+++ resources/views/template_admin/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
   <div class="footer-left">
-    Copyright &copy; 2021 <div class="bullet"></div> Aplikasi Sekolah Luar Biasa
+    Copyright &copy; 2021 <div class="bullet"></div> Aplikasi Magang
   </div>
   <div class="footer-right">
     2.3.0
